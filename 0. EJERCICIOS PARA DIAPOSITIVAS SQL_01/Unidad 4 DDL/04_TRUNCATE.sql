e65bb22d4133e05b026c481607b920a820fe1301
AlbertoRiveraLobato <alberto.riveralobato@thepower.education>
2025-09-09 19:15:10 +0200

    ok

---
+++ 0. EJERCICIOS PARA DIAPOSITIVAS SQL_01/Unidad 4 DDL/04_TRUNCATE.sql
@@ -0,0 +1,2 @@
+-- Vaciar todos los registros de una tabla, pero mantener su estructura
+TRUNCATE TABLE comandos.empresa_nueva;
---
+++ 0. EJERCICIOS PARA DIAPOSITIVAS SQL_01/Unidad 4 DDL/06_SHOW_USE_DESCRIBE.sql
@@ -0,0 +1,14 @@
+-- Mostrar todas las bases de datos
+SHOW DATABASES;
+
+-- Mostrar todas las tablas de la base de datos actual
+SHOW TABLES;
+
+-- Mostrar la estructura de una tabla
+DESCRIBE comandos.empresa_nueva;
+
+-- Usar una base de datos
+USE MiBaseDeDatos;
+
+-- Mostrar columnas de una tabla
+SHOW COLUMNS FROM comandos.empresa_nueva;
